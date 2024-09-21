8f50fe5bb9d1f76524f1f66d66c31f0996f5d4a7
ishandhanani <ishandhanani@gmail.com>
2024-09-21 12:49:02 -0700

    proper filtering based on your permissions

--- cmd/gpu/gpulist.go
+++ cmd/gpu/gpulist.go
@@ -1,7 +1,12 @@
+// the logic used to filter available clusters comes from the NGC frontend for NVCF
 package gpu
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/brevdev/nvcf/api"
+	"github.com/brevdev/nvcf/collections"
 	"github.com/brevdev/nvcf/config"
 	"github.com/brevdev/nvcf/output"
 	"github.com/spf13/cobra"
@@ -12,7 +17,7 @@ func gpuListCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "list",
 		Short: "List available GPUs",
-		Long:  "List available GPUs in NVCF. Note this is still in beta and lists all available GPUs ",
+		Long:  "List available GPUs in NVCF. Note this is still in beta and lists all available GPUs",
 		RunE:  runGpuList,
 	}
 
@@ -23,19 +28,17 @@ func gpuListCmd() *cobra.Command {
 }
 
 func runGpuList(cmd *cobra.Command, args []string) error {
-	client := api.NewClient(config.GetAPIKey())
-
-	gpus, err := client.ClusterGroups.List(cmd.Context())
-	if err != nil {
-		return output.Error(cmd, "Error listing cluster groups", err)
-	}
-
 	backend, _ := cmd.Flags().GetString("backend")
 	gpuType, _ := cmd.Flags().GetString("gpu")
 
-	filteredGroups := filterClusterGroups(gpus.ClusterGroups, backend, gpuType)
+	availableInstanceTypes, err := GetAvailableInstanceTypes(cmd.Context(), backend, gpuType)
+	if err != nil {
+		return output.Error(cmd, "Error getting available instance types", err)
+	}
 
-	output.GPUs(cmd, filteredGroups)
+	filteredClusterGroups := filterClusterGroups(availableInstanceTypes, backend, gpuType)
+
+	output.GPUs(cmd, filteredClusterGroups)
 
 	return nil
 }
@@ -64,4 +67,86 @@ func filterClusterGroups(groups []nvcf.ClusterGroupsResponseClusterGroup, backen
 	}
 
 	return filtered
+}
+
+func buildNVCFOrgInformationURL(orgID string) string {
+	return fmt.Sprintf("/v3/orgs/%s/nvcf", orgID)
+}
+
+func getNVCFOrgInformation(orgID string) (OrgClusterGroupsResponse, error) {
+	client := api.NewClient(config.GetAPIKey())
+	url := buildNVCFOrgInformationURL(orgID)
+	var res OrgClusterGroupsResponse
+	err := client.Get(context.Background(), url, nil, &res)
+	if err != nil {
+		return OrgClusterGroupsResponse{}, err
+	}
+	return res, nil
+}
+
+func GetNVCFClusterGroups(ctx context.Context) (*nvcf.ClusterGroupsResponse, error) {
+	client := api.NewClient(config.GetAPIKey())
+	availableCluster, err := client.ClusterGroups.List(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return availableCluster, nil
+}
+
+type OrgClusterGroupsResponse struct {
+	BillingAccountID string        `json:"billingAccountId"`
+	Clusters         []Cluster     `json:"clusters"`
+	RequestStatus    RequestStatus `json:"requestStatus"`
+}
+
+type Cluster struct {
+	Cluster          string `json:"cluster"`
+	GpuType          string `json:"gpuType"`
+	InstanceType     string `json:"instanceType"`
+	MaxInstances     int    `json:"maxInstances"`
+	CurrentInstances int    `json:"currentInstances"`
+}
+
+type RequestStatus struct {
+	StatusCode string `json:"statusCode"`
+}
+
+func GetAvailableInstanceTypes(ctx context.Context, backend, gpuType string) ([]nvcf.ClusterGroupsResponseClusterGroup, error) {
+	orgInfo, err := getNVCFOrgInformation(config.GetOrgID())
+	if err != nil {
+		return nil, err
+	}
+	clusterGroups, err := GetNVCFClusterGroups(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a map of cluster names to their GPU configurations
+	gpuConfigs := make(map[string]bool)
+	for _, cluster := range orgInfo.Clusters {
+		gpuConfigs[cluster.Cluster] = true
+	}
+
+	filteredGroups := collections.Filter(clusterGroups.ClusterGroups, func(cluster nvcf.ClusterGroupsResponseClusterGroup) bool {
+		// Check if the cluster is owned by the organization (BYOC)
+		if cluster.NcaID == orgInfo.BillingAccountID {
+			return true
+		}
+
+		// Check if the cluster is shared from another account but authorized for use
+		for _, authorizedNcaID := range cluster.AuthorizedNcaIDs {
+			if authorizedNcaID == orgInfo.BillingAccountID {
+				return true
+			}
+		}
+
+		// Check if the cluster is a shared gated cluster and has GPU resources available
+		if collections.ListContains(cluster.AuthorizedNcaIDs, "*") && gpuConfigs[cluster.Name] {
+			return true
+		}
+
+		return false
+	})
+
+	return filteredGroups, nil
 }
---
+++ collections/collections.go
@@ -0,0 +1,32 @@
+package collections
+
+func Filter[T any](list []T, f func(T) bool) []T {
+	result := []T{}
+	for _, item := range list {
+		if f(item) {
+			result = append(result, item)
+		}
+	}
+	return result
+}
+
+func ListContains[K comparable](list []K, item K) bool {
+	return ListHas(list, func(l K) bool { return l == item })
+}
+
+func ListHas[K any](list []K, has func(l K) bool) bool {
+	k := Find(list, has)
+	if k != nil {
+		return true
+	}
+	return false
+}
+
+func Find[T any](list []T, f func(T) bool) *T {
+	for _, item := range list {
+		if f(item) {
+			return &item
+		}
+	}
+	return nil
+}
